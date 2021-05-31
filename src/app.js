30858271cb798d822ad569d281220d2977002a5e
nahimrgz <nahimrgz@ingenieros.com>
2021-05-31 00:34:38 -0600

    correcion editar archivo

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.set("json spaces", 4);
 
 // Middlewares
 const corsOptions = {
-  origin: "http://localhost:4000",
+  origin: "http://localhost:4200",
 };
 app.use(cors(corsOptions));
 app.use(helmet());
--- src/controllers/datos.controller.js
+++ src/controllers/datos.controller.js
@@ -94,14 +94,54 @@ export const getDatos = async (req, res)=> {
 export const getDatoByID = async (req, res)=> {
     const {datoId} = req.params;
 
-    const dato = await Data.findByID();
+    // const dato = await Data.findByID(datoId);
+    const dato = await Data.findById(datoId)
     res.status(200).json(dato);
 };
 
 export const updateDatoById =async (req, res)=> {
-    const {datoId} = req.params;
-    await Data.findByIdAndUpdate(datoId);
-    res.status(200).json();
+    const datoId = req.params.datoId;
+    const body = req.body;
+    console.log('datoId', datoId);
+    console.log('body: ', body)
+
+    // await Data.findByIdAndUpdate(datoId, { $set: {departamento: 'flippy gay'}});
+    await Data.findOneAndUpdate({_id: datoId}, {$set: {
+        departamento:body.departamento, 
+        avance:body.avance, 
+        status:body.status, 
+        nuc:body.nuc, 
+        oficio:body.oficio, 
+        equipo:body.equipo, 
+        unidad:body.unidad, 
+        zona:body.zona, 
+        fechaD:body.fechaD, 
+        hora:body.hora, 
+        fechaR:body.fechaR, 
+        id_del:body.id_del, 
+        delito:body.delito, 
+        num_if:body.num_if, 
+        fechaC:body.fechaC, 
+        lic:body.lic, 
+        agente:body.agente, 
+        fechaRecol:body.fechaRecol, 
+        dir:body.dir, 
+        dis:body.dis, 
+        evidencia:body.evidencia, 
+        banco:body.banco, 
+        marcaEqui:body.marcaEqui, 
+        modeloEqui:body.modeloEqui, 
+        serieEqui:body.serieEqui, 
+        marcaAlma:body.marcaAlma, 
+        modeloAlma:body.modeloAlma, 
+        serieAlma:body.serieAlma, 
+        md5:body.md5, 
+        sha1:body.sha1, 
+        swImagen:body.swImagen, 
+        swArte:body.swArte, 
+        swInfo:body.swInfo,
+    }})
+    res.status(200).json('Registro actualizado OK');
 };
 
 export const getDatoByNuc = async (req, res) => {
